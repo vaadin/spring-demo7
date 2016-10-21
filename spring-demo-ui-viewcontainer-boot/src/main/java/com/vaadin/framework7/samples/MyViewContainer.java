4065104267a9fc46062a440b959522407179c3f3
elmot <elmot@vaadin.com>
2016-10-21 14:39:52 +0300

    Remove unnecessary @UIScope

--- spring-demo-ui-viewcontainer-boot/src/main/java/com/vaadin/framework7/samples/MyViewContainer.java
+++ spring-demo-ui-viewcontainer-boot/src/main/java/com/vaadin/framework7/samples/MyViewContainer.java
@@ -16,7 +16,6 @@
 package com.vaadin.framework7.samples;
 
 import com.vaadin.spring.annotation.SpringComponent;
-import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.spring.annotation.ViewContainer;
 import com.vaadin.ui.CssLayout;
 
@@ -26,7 +25,6 @@ import com.vaadin.ui.CssLayout;
  * @author Vaadin Ltd
  */
 @SpringComponent
-@UIScope
 @ViewContainer
 public class MyViewContainer extends CssLayout{
 }
